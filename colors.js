2976b80685cc3d3b3ce6ba8e3e87ab6c8d699d5e
Mike Larsson <mikelarssonftw@gmail.com>
2014-04-14 18:32:23 -0400

    better palette dumper

--- colors.js
+++ colors.js
@@ -2,6 +2,26 @@ var charm = require('charm')();
 charm.pipe(process.stdout);
 charm.reset();
 
-for (var i = 0; i <= 255; i++) {
-  charm.display("dim").foreground(i).write(i + '    ');
+charm.write('standard colors:\t');
+for (var i = 0; i < 8; i++) {
+  charm.display('reset').foreground(i).write(i.toString()).background(i).write('\t');
 }
+
+charm.display('reset').write('\n\nhigh intensity colors:\t');
+for (var i = 8; i < 16; i++) {
+  charm.display('reset').foreground(i).write(i.toString()).background(i).write('\t');
+}
+
+charm.display('reset').write('\n\n216 colors:');
+for (var i = 16; i < 232; i++) {
+  if (i % 16 === 0) charm.write('\n');
+  charm.display('reset').foreground(i).write(i.toString()).background(i).write('\t');
+}
+
+charm.display('reset').write('\n\ngrayscale:\t');
+for (var i = 232; i < 256; i++) {
+  charm.display('reset').foreground(i).write(i.toString()).background(i).write('\t');
+}
+
+charm.write('\n\n');
+charm.display('reset');
